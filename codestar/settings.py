83bea62682803b2263c80cd9804f6df1e131100f
STnobile <stnobile0@gmail.com>
2023-05-15 18:26:50 +0000

    depo4

--- codestar/settings.py
+++ codestar/settings.py
@@ -133,7 +133,7 @@ USE_TZ = True
 
 MEDIA_URL = '/media/'
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
- 
+
 STATIC_URL = '/static/'
 STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
