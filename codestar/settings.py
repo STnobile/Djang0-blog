aa0dc4db8e994bce218a4415c2792f199b941db8
STnobile <stnobile0@gmail.com>
2023-05-16 09:18:59 +0000

    add summernote

--- codestar/settings.py
+++ codestar/settings.py
@@ -26,7 +26,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('my_super^Wanda')
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -45,8 +45,9 @@ INSTALLED_APPS = [
     'cloudinary_storage',
     'django.contrib.staticfiles',
     'cloudinary',
+    'django_summernote',
     'blog',
-    'django-bloggf',
+    
 ]
 
 MIDDLEWARE = [
