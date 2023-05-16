e49164661db4d7d1663bb6b5a5eaa39e0379f31b
STnobile <stnobile0@gmail.com>
2023-05-16 09:37:41 +0000

    debug false

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+
+
+# urlpatterns = [
+#   path('admin/', admin.site.urls),
+#  path('summernote/', include('dingo_summernote.urls')),]
